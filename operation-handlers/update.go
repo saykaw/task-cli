084ed740387bcc7de0de9d69c76285677821d118
Sayali <saykaw16@gmail.com>
2025-09-20 11:16:55 +0530

    update operation: done (with updated time)

--- operation-handlers/update.go
+++ operation-handlers/update.go
@@ -1,6 +1,8 @@
 package operationhandlers
 
 import (
+	"time"
+
 	filehandlers "github.com/saykaw/task-cli/file-handlers"
 )
 
@@ -9,6 +11,7 @@ func Update(id int, taskDescription string) {
 	for i, loadedTask := range loadedTasks {
 		if loadedTask.Id == id {
 			loadedTasks[i].TaskDescription = taskDescription
+			loadedTasks[i].UpdatedAt = time.Now()
 		}
 	}
 	filehandlers.SaveFile(loadedTasks)
